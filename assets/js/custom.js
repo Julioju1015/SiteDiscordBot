627200a1fa765e354b58c0e51d4baa70457f5c87
julioju <41541427+juliojugithub@users.noreply.github.com>
2020-05-25 10:43:05 +0200

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+var options = {
+    loop: true,
+    typeSpeed: 60,
+    backSpeed: 20,
+    backDelay: 1000,
+    showCursor: false,
+    startDelay: 90,
+    strings: ["Ceci", "est", "un", "open source", "développer", "par", "julioju#7775", "Ceci est un open source développer par julioju#7775"]
+}
+var typed = new Typed(".commands", options);
